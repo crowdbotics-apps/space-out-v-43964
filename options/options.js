baba3584ed925b4a389adc956bb8beee350e8ac3
Crowdbotics <team@crowdbotics.com>
2023-10-16 12:50:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "space-out-v-43964",
+  url: "https://space-out-v-43964.botics.co",
+  api: "https://space-out-v-43964.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
